acd7e1e81c714277d7f5e7a54078fef5088c83c1
ippimi <93322305+ippimi@users.noreply.github.com>
2021-10-29 09:49:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function renderPlaces(places) {
        model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '100 100 100');
-       model.setAttribute('position', '0 25 0');
+       
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
        });
