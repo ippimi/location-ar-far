3aebbcbeee43a9575dff0afc9a60baa5d5448ffc
ippimi <93322305+ippimi@users.noreply.github.com>
2021-10-29 09:57:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,10 +26,7 @@ function renderPlaces(places) {
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-       model.setAttribute('rotation', '0 180 0');
-       model.setAttribute('animation-mixer', '');
        model.setAttribute('scale', '100 100 100');
-       
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
        });
